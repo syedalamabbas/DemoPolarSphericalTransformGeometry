0ef6873440e9f3d08cb9c376f1ba754e504cf665
SyedAlamAbbas <admin@example.com>
2016-11-18 17:27:36 -0500

    Did some code clean up

--- Source/CppBeamEmitter/BeamWork/BeamEmitterBaseActor.cpp
+++ Source/CppBeamEmitter/BeamWork/BeamEmitterBaseActor.cpp
@@ -3,17 +3,14 @@
 #include "CppBeamEmitter.h"
 #include "BeamEmitterBaseActor.h"
 
-
 // Sets default values
 ABeamEmitterBaseActor::ABeamEmitterBaseActor()
 {
 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = false;
 
-
 	WhereToSpawn = CreateDefaultSubobject<UBoxComponent>(TEXT("WhereToSpawnRoot"));
 	RootComponent = WhereToSpawn;
-
 
 	BeamParticleSystem = CreateDefaultSubobject<UParticleSystemComponent>(TEXT("DefaultEmptyParticleSystem"));
 	BeamParticleSystem->SetupAttachment(RootComponent);
@@ -23,8 +20,6 @@ ABeamEmitterBaseActor::ABeamEmitterBaseActor()
 void ABeamEmitterBaseActor::BeginPlay()
 {
 	Super::BeginPlay();
-	BeamParticleSystem->InitializeSystem();
-	BeamParticleSystem->ActivateSystem();
 	CreateDesiredBeamPattern();
 }
 
@@ -32,9 +27,7 @@ void ABeamEmitterBaseActor::BeginPlay()
 void ABeamEmitterBaseActor::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-
 }
-
 
 FVector ABeamEmitterBaseActor::GetOriginInVolume()
 {
@@ -43,88 +36,38 @@ FVector ABeamEmitterBaseActor::GetOriginInVolume()
 
 void ABeamEmitterBaseActor::CreateDesiredBeamPattern()
 {
-	int32 NumberOfBeams = 10;
-	//SpawnLocationsSource.SetNum(NumberOfBeams, false);
-	int32 BeamLength = 200;
-	int32 ZOffset = 20;
-
-	BeamParticleSystem->Activate();
+	int32 NumberOfBeams = 3;
+	int32 BeamLength = 400;
+	int32 ZOffset = 10;
 
 	FVector BoxLocation = GetOriginInVolume();
-	FName SocketName = NAME_None;
-
 	FVector TargetPoint;
 
 
-	for (int32 j = 0; j < NumberOfBeams+1; j++)
-	{ 
-		for (int32 i = 0; i < NumberOfBeams+2; i++)  // Along X Axis
-		{
-			TargetPoint.X = BoxLocation.X;                 // Constant
-			TargetPoint.Y = BoxLocation.Y + j*ZOffset;                 // Constant
-			TargetPoint.Z = BoxLocation.Z + i*ZOffset;   // Moving in Straight Line
-
-			if (BeamParticleSystem)
-			{
-				UParticleSystemComponent* CurrentEmitter = UGameplayStatics::SpawnEmitterAttached(
-					BeamParticleSystem->Template, //UParticleSystem*
-					RootComponent,
-					SocketName,
-					TargetPoint, //relative offset 
-					FRotator(0,0, 0), //relative rotation
-					EAttachLocation::KeepWorldPosition,
-					true //auto delete on completion
-				);
-			}
-		}
-	}
-
-	for (int32 j = 0; j < NumberOfBeams+1; j++)
+	for (int32 i = 0; i < NumberOfBeams; i++)  // Along X Axis
 	{
-		for (int32 i = 0; i < NumberOfBeams+2; i++)  // Along X Axis
-		{
-			TargetPoint.X = BoxLocation.X + i*ZOffset;                 // Constant
-			TargetPoint.Y = BoxLocation.Y + j*ZOffset;                 // Constant
-			TargetPoint.Z = BoxLocation.Z ;   // Moving in Straight Line
+		TargetPoint.X = BoxLocation.X;                 // Constant
+		TargetPoint.Y = BoxLocation.Y;                 // Constant
+		TargetPoint.Z = BoxLocation.Z + i*ZOffset;   // Moving in Straight Line
 
-			if (BeamParticleSystem)
-			{
-				UParticleSystemComponent* CurrentEmitter = UGameplayStatics::SpawnEmitterAttached(
-					BeamParticleSystem->Template, //UParticleSystem*
-					RootComponent,
-					SocketName,
-					TargetPoint, //relative offset 
-					FRotator(90, 90, 0), //relative rotation
-					EAttachLocation::KeepWorldPosition,
-					true //auto delete on completion
-				);
-			}
+		if (BeamParticleSystem)
+		{
+			UParticleSystemComponent* CurrentEmitter = UGameplayStatics::SpawnEmitterAtLocation(
+				GetWorld(),
+				BeamParticleSystem->Template,
+				TargetPoint,
+				FRotator::ZeroRotator,
+				true);
+
+			CurrentEmitter->InitializeSystem();
+			CurrentEmitter->SetBeamSourcePoint(0, TargetPoint, 0);
+
+			FVector NewPoint = FVector(TargetPoint);
+			NewPoint.X = NewPoint.X + BeamLength;  // Beam length in X direction 
+			CurrentEmitter->SetBeamTargetPoint(0, NewPoint, 0);
+			CurrentEmitter->ActivateSystem();
 		}
 	}
-	
 
-	for (int32 j = 0; j < NumberOfBeams+1; j++)
-	{
-		for (int32 i = 0; i < NumberOfBeams+2; i++)  // Along X Axis
-		{
-			TargetPoint.X = BoxLocation.X + i*ZOffset;                 // Constant
-			TargetPoint.Y = BoxLocation.Y ;                 // Constant
-			TargetPoint.Z = BoxLocation.Z + j*ZOffset;   // Moving in Straight Line
-			if (BeamParticleSystem)
-			{
-				UParticleSystemComponent* CurrentEmitter = UGameplayStatics::SpawnEmitterAttached(
-					BeamParticleSystem->Template, //UParticleSystem*
-					RootComponent,
-					SocketName,
-					TargetPoint, //relative offset 
-					FRotator(0, 90, 90), //relative rotation
-					EAttachLocation::KeepWorldPosition,
-					true //auto delete on completion
-				);
-			}
-		}
-	}
-	
-	
 }
 
