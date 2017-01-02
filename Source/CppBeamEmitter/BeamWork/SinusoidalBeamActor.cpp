83a19056163c506ac69a996e24f00833c3449cf1
Syed Alam Abbas <syedalamabbas@knights.ucf.edu>
2017-01-01 21:47:23 -0800

    Added Sinusoidal model using beams

---
+++ Source/CppBeamEmitter/BeamWork/SinusoidalBeamActor.cpp
@@ -0,0 +1,90 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "CppBeamEmitter.h"
+#include "SinusoidalBeamActor.h"
+
+// Sets default values
+ASinusoidalBeamActor::ASinusoidalBeamActor()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	WhereToSpawn = CreateDefaultSubobject<UBoxComponent>(TEXT("WhereToSpawnRoot"));
+	RootComponent = WhereToSpawn;
+
+	BeamParticleSystem = CreateDefaultSubobject<UParticleSystemComponent>(TEXT("DefaultEmptyParticleSystem"));
+	BeamParticleSystem->SetupAttachment(RootComponent);
+
+	// Initial values 
+	NumberOfAtoms = 100;
+	SinusoidalParams.SetAmplitude(50.f);
+	SinusoidalParams.SetFrequency(0.001f);
+	SinusoidalParams.SetPhase(0.0f);
+	SinusoidalParams.SetWaveType(ESinusoidType::SINE);
+	SinusoidalParams.SetTimeDelta(3.0f);
+
+}
+
+// Called when the game starts or when spawned
+void ASinusoidalBeamActor::BeginPlay()
+{
+	Super::BeginPlay();
+	CreateDesiredBeamPattern();
+}
+
+// Called every frame
+void ASinusoidalBeamActor::Tick( float DeltaTime )
+{
+	Super::Tick( DeltaTime );
+}
+
+FVector ASinusoidalBeamActor::GetOriginInVolume()
+{
+	return WhereToSpawn->GetComponentLocation();
+}
+
+void ASinusoidalBeamActor::CreateDesiredBeamPattern()
+{
+	SpawnLocations.SetNum(NumberOfAtoms, false);
+	FVector BoxLocation = GetOriginInVolume();
+
+	// Calculating locations based on Sinuosidal Patterns 
+	for (int32 i = 0; i < NumberOfAtoms; i++)
+	{
+		SpawnLocations[i].X = BoxLocation.X + i*SinusoidalParams.TimeDelta;   // Moving in Straight Line
+		SpawnLocations[i].Y = BoxLocation.Y;                 // Constant
+		float ZOutput = 0.0f;
+		if (SinusoidalParams.WaveType == ESinusoidType::SINE)
+		{
+			ZOutput = SinusoidalParams.Amplitude * FMath::Sin(2 * PI* SinusoidalParams.Frequency *i* SinusoidalParams.TimeDelta + SinusoidalParams.Phase);
+		}
+		if (SinusoidalParams.WaveType == ESinusoidType::COSINE)
+		{
+			ZOutput = SinusoidalParams.Amplitude * FMath::Cos(2 * PI* SinusoidalParams.Frequency *i* SinusoidalParams.TimeDelta + SinusoidalParams.Phase);
+		}
+		SpawnLocations[i].Z = BoxLocation.Z + ZOutput;   // Moving in Sinusoidally
+	}
+
+	// Spawning Emitters 
+	UParticleSystem* TemplateParticleSystem = BeamParticleSystem->Template;
+	FVector BeamSourcePoint;
+	FVector BeamTargetPoint;
+	for (int32 i = 0; i < NumberOfAtoms; i++)
+	{
+		BeamSourcePoint = SpawnLocations[i];
+		if ((i != NumberOfAtoms - 1) && BeamParticleSystem)
+		{
+			BeamTargetPoint = SpawnLocations[i + 1];
+			UParticleSystemComponent* CurrentEmitter = UGameplayStatics::SpawnEmitterAtLocation(
+				GetWorld(),
+				BeamParticleSystem->Template,
+				BeamSourcePoint,
+				FRotator::ZeroRotator,
+				true);
+			CurrentEmitter->SetBeamSourcePoint(0, BeamSourcePoint, 0);
+			CurrentEmitter->SetBeamTargetPoint(0, BeamTargetPoint, 0);
+		}
+
+	}
+
+}
---
+++ Source/CppBeamEmitter/BeamWork/SinusoidalBeamActor.h
@@ -0,0 +1,157 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/Actor.h"
+#include "SinusoidalBeamActor.generated.h"
+
+UENUM(BlueprintType)		//"BlueprintType" is essential to include
+enum class ESinusoidType : uint8
+{
+	SINE,
+	COSINE
+};
+
+//If you want this to appear in BP, make sure to use this instead
+USTRUCT(BlueprintType)
+//USTRUCT()
+struct FSinusoidalParams
+{
+	GENERATED_USTRUCT_BODY()
+
+		// So to simplify your life for later debugging, always use UPROPERTY()
+		UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "SinusoidalParams")
+		ESinusoidType WaveType;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "SinusoidalParams")
+		float Amplitude; // Tha Amplitude
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "SinusoidalParams")
+		float Frequency; // Tha Frequency
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "SinusoidalParams", meta = (ClampMin = "0.0", ClampMax = "6.2831853071795864", UIMin = "0.0", UIMax = "6.2831853071795864"))
+		float Phase; // Tha Phase - range from 0 - 2*PI
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "SinusoidalParams")
+		float TimeDelta; // Tha Time Delay or Delta
+
+						 //Get WaveType
+	ESinusoidType GetWaveType()
+	{
+		return WaveType;
+	}
+
+
+	//Get
+	float GetAmplitude()
+	{
+		return Amplitude;
+	}
+
+	//Get
+	float GetFrequency()
+	{
+		return Frequency;
+	}
+
+	//Get
+	float GetPhase()
+	{
+		return Phase;
+	}
+
+	//Get
+	float GetTimeDelta()
+	{
+		return TimeDelta;
+	}
+
+	//Set
+	void SetWaveType(const ESinusoidType NewValueWave)
+	{
+		WaveType = NewValueWave;
+	}
+
+
+	//Set
+	void SetAmplitude(const float NewValueAmplitude)
+	{
+		Amplitude = NewValueAmplitude;
+	}
+
+	//Set
+	void SetFrequency(const float NewValueFrequency)
+	{
+		Frequency = NewValueFrequency;
+	}
+
+	//Set
+	void SetPhase(const float NewValuePhase)
+	{
+		Phase = NewValuePhase;
+	}
+
+	//Set
+	void SetTimeDelta(const float NewValueTimeDelta)
+	{
+		TimeDelta = NewValueTimeDelta;
+	}
+
+	//Constructor
+	FSinusoidalParams()
+	{
+		//Always initialize your USTRUCT variables!
+		//   exception is if you know the variable type has its own default constructor
+		WaveType = ESinusoidType::SINE;
+		Amplitude = 0;
+		Frequency = 0;
+		Phase = 0;
+		TimeDelta = 0;
+	}
+};
+
+UCLASS()
+class CPPBEAMEMITTER_API ASinusoidalBeamActor : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ASinusoidalBeamActor();
+
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	
+	// Called every frame
+	virtual void Tick( float DeltaSeconds ) override;
+
+	UPROPERTY(EditAnywhere, Category = "Spawning")
+		UParticleSystemComponent* BeamParticleSystem;
+
+	/*UPROPERTY(EditAnywhere, Category = "Spawning")
+	TSubclassOf<ADummyActor> WhatToSpawn;*/
+
+	FORCEINLINE UBoxComponent* GetWhereToSpawn() const { return WhereToSpawn; }
+
+	UFUNCTION(BlueprintPure, Category = "Spawning")
+		FVector GetOriginInVolume();
+
+
+private: 
+	void CreateDesiredBeamPattern();
+
+
+protected:
+	// Where to Spawn
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Spawning", meta = (AllowPrivateAccess = "true"))
+	UBoxComponent* WhereToSpawn;            // This is the anchor
+
+	UPROPERTY(EditAnywhere, Category = "Spawning")
+	TArray<FVector> SpawnLocations;			// These are the actual locations
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Spawning")
+	FSinusoidalParams SinusoidalParams;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Spawning")
+	int32 NumberOfAtoms;
+};
